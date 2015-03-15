80959715f52a583b9656263f41cbea1cf6de91cb
schlueter <bs@bschlueter.com>
2015-03-15 18:57:46 -0400

    Create mysql_master recipe

---
+++ recipes/mysql_master.rb
@@ -0,0 +1,29 @@
+##
+#    Copyright (C) 2015 Brandon Schlueter
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+mysql_service 'default' do
+  port '3306'
+  version '5.5'
+  initial_root_password node['failover_wordpress']['database']['master']['initial_root_password']
+  action [:create, :start]
+end
+
+mysql_config 'default' do
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+  action :create
+end
