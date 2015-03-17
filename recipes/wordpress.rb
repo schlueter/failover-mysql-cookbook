3070de2f64d160e32aef8fd68e845b52dda8fdf7
schlueter <bs@bschlueter.com>
2015-03-16 22:10:53 -0400

    Lint/correct logic in comment

--- recipes/wordpress.rb
+++ recipes/wordpress.rb
@@ -17,34 +17,34 @@
 
 include_recipe "wordpress::#{node['failover_wordpress']['webserver']['server']}"
 
-# Override the wordpress ckbk's nginx because of a chef bug which makes controlling the value
-# of the wordpress' user's password in attributes difficult.
-# https://tickets.opscode.com/browse/CHEF-2945#comment-50680
-
 primary_db = node['failover_wordpress']['database']['master']
+
+# Override the wordpress ckbk's config because of a chef bug which makes controlling
+# the value of the wordpress' user's password in attributes difficult.
+# https://tickets.opscode.com/browse/CHEF-2945#comment-50680
 
 template "#{node['wordpress']['dir']}/wp-config.php" do
   source 'wp-config.php.erb'
   mode node['wordpress']['config_perms']
   variables(
-    :db_name           => primary_db['name'],
-    :db_user           => primary_db['user'],
-    :db_password       => primary_db['pass'],
-    :db_host           => primary_db['host'],
-    :db_prefix         => node['wordpress']['db']['prefix'],
-    :db_charset        => node['wordpress']['db']['charset'],
-    :db_collate        => node['wordpress']['db']['collate'],
-    :auth_key          => node['wordpress']['keys']['auth'],
-    :secure_auth_key   => node['wordpress']['keys']['secure_auth'],
-    :logged_in_key     => node['wordpress']['keys']['logged_in'],
-    :nonce_key         => node['wordpress']['keys']['nonce'],
-    :auth_salt         => node['wordpress']['salt']['auth'],
-    :secure_auth_salt  => node['wordpress']['salt']['secure_auth'],
-    :logged_in_salt    => node['wordpress']['salt']['logged_in'],
-    :nonce_salt        => node['wordpress']['salt']['nonce'],
-    :lang              => node['wordpress']['languages']['lang'],
-    :allow_multisite   => node['wordpress']['allow_multisite'],
-    :wp_config_options => node['wordpress']['wp_config_options']
+    db_name: primary_db['name'],
+    db_user: primary_db['app_user'],
+    db_password: primary_db['app_pass'],
+    db_host: primary_db['host'],
+    db_prefix: node['wordpress']['db']['prefix'],
+    db_charset: node['wordpress']['db']['charset'],
+    db_collate: node['wordpress']['db']['collate'],
+    auth_key: node['wordpress']['keys']['auth'],
+    secure_auth_key: node['wordpress']['keys']['secure_auth'],
+    logged_in_key: node['wordpress']['keys']['logged_in'],
+    nonce_key: node['wordpress']['keys']['nonce'],
+    auth_salt: node['wordpress']['salt']['auth'],
+    secure_auth_salt: node['wordpress']['salt']['secure_auth'],
+    logged_in_salt: node['wordpress']['salt']['logged_in'],
+    nonce_salt: node['wordpress']['salt']['nonce'],
+    lang: node['wordpress']['languages']['lang'],
+    allow_multisite: node['wordpress']['allow_multisite'],
+    wp_config_options: node['wordpress']['wp_config_options']
   )
   owner node['wordpress']['install']['user']
   group node['wordpress']['install']['group']
