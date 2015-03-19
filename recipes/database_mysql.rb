5439cd001c546d55438abcef70fe4a757da9b95d
schlueter <bs@bschlueter.com>
2015-03-19 08:22:42 -0400

    Add failover to mysql recipe

--- recipes/database_mysql.rb
+++ recipes/database_mysql.rb
@@ -19,79 +19,93 @@ db = node['failover-mysql']['database']
 
 is_slave = true unless db['master_host'].nil?
 
-mysql_service db['instance_name'] do
-  version '5.5'
-  bind_address db['host']
-  initial_root_password db['root_password']
-  action [:create, :start]
-end
-
-mysql_config db['instance_name'] do
-  source 'mysite.cnf.erb'
-  notifies :restart, 'mysql_service[default]'
-  variables(
-    server_id: db['server_id'],
-    host: db['host']
-  )
-  action :create
-end
-
-socket = "/var/run/mysql-#{db['instance_name']}/mysqld.sock"
-
-link '/var/run/mysqld/mysqld.sock' do
-  to socket
-  not_if 'test -f /var/run/mysqld/mysqld.sock'
-end
-
 mysql_connection_info = {
   host: db['host'],
   username: 'root',
   password: db['root_password']
 }
 
-# Used by database resources below
-mysql2_chef_gem 'default' do
-  action :install
-end
+master_mysql_connection_info = {
+  host: db['master_host'],
+  username: db['slave_user'],
+  password: db['slave_pass']
+}
 
-# This:
-# mysql> GRANT REPLICATION CLIENT ON *.* TO 'slave'@'192.168.10.12';
-# then get the log file name:
-# mysql -h 192.168.10.11 -uslave -pslave -e "show master status" -s | tail -n 1 | awk {'print $1'}
-# and log pos:
-# mysql -h 192.168.10.11 -uslave -pslave -e "show master status" -s | tail -n 1 | awk {'print $2'}
-if is_slave
-  master_mysql_connection_info = {
-    host: db['master_host'],
-    username: db['slave_user'],
-    password: db['slave_pass']
-  }
+if db['failover']
+
+  log "connection #{mysql_connection_info}"
+  log "slave_connection #{master_mysql_connection_info}"
+  log "slave_clients #{db['slaves']}"
 
   mysql_database "#{db['name']}" do
-    connection master_mysql_connection_info
-    slave_connection mysql_connection_info
-    action :slave
+    connection mysql_connection_info
+    slave_connection master_mysql_connection_info
+    slave_clients db['slaves']
+    action :promote
   end
-end
+else
+  mysql_service db['instance_name'] do
+    version '5.5'
+    bind_address db['host']
+    initial_root_password db['root_password']
+    action [:create, :start]
+  end
 
-mysql_database db['name'] do
-  connection mysql_connection_info
-  action :create
-end
+  mysql_config db['instance_name'] do
+    source 'mysite.cnf.erb'
+    notifies :restart, 'mysql_service[default]'
+    variables(
+      server_id: db['server_id'],
+      host: db['host']
+    )
+    action :create
+  end
 
-mysql_database_user db['app_user'] do
-  connection mysql_connection_info
-  password db['app_pass']
-  database_name db['name']
-  host node['failover-mysql']['web']['host']
-  privileges [:all]
-  action [:create, :grant]
-end
+  socket = "/var/run/mysql-#{db['instance_name']}/mysqld.sock"
 
-mysql_database_user db['slave_user'] do
-  connection mysql_connection_info
-  password db['slave_pass']
-  host db['slave_host']
-  privileges ['REPLICATION SLAVE', 'REPLICATION CLIENT']
-  action [:create, :grant]
+  link '/var/run/mysqld/mysqld.sock' do
+    to socket
+    not_if 'test -f /var/run/mysqld/mysqld.sock'
+  end
+
+  # Used by database resources below
+  mysql2_chef_gem 'default' do
+    action :install
+  end
+
+  # This:
+  # mysql> GRANT REPLICATION CLIENT ON *.* TO 'slave'@'192.168.10.12';
+  # then get the log file name:
+  # mysql -h 192.168.10.11 -uslave -pslave -e "show master status" -s | tail -n 1 | awk {'print $1'}
+  # and log pos:
+  # mysql -h 192.168.10.11 -uslave -pslave -e "show master status" -s | tail -n 1 | awk {'print $2'}
+  if is_slave
+    mysql_database "#{db['name']}" do
+      connection master_mysql_connection_info
+      slave_connection mysql_connection_info
+      action :slave
+    end
+  end
+
+  mysql_database db['name'] do
+    connection mysql_connection_info
+    action :create
+  end
+
+  mysql_database_user db['app_user'] do
+    connection mysql_connection_info
+    password db['app_pass']
+    database_name db['name']
+    host node['failover-mysql']['web']['host']
+    privileges [:all]
+    action [:create, :grant]
+  end
+
+  mysql_database_user db['slave_user'] do
+    connection mysql_connection_info
+    password db['slave_pass']
+    host db['slave_host']
+    privileges ['REPLICATION SLAVE', 'REPLICATION CLIENT']
+    action [:create, :grant]
+  end
 end
